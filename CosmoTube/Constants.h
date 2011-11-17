9df2e229a88625418878b7c6481e7042e47f8f4f
Peter Shih <ptshih@gmail.com>
2011-11-17 18:34:37 -0500

    db define name

--- CosmoTube/Constants.h
+++ CosmoTube/Constants.h
@@ -1,2 +1,10 @@
 #import "PSConstants.h"
-#define SQLITE_DB @"CosmoTube_v1"
+
+// SQLite DB filename
+#define SQLITE_DB_NAME @"CosmoTube_v1"
+#define SQLITE_DB_SCHEMA_VERSION @"1"
+
+// Youtube GData API Developer Key
+#define YOUTUBE_KEY @"AI39si6JNQLnJmOPMRIG9Lcp9iM5Yt9YLiFsXhJ_cZUZRgrd2m8AGF-xLwjip7_TPRosYF7d2JaJDVdtlc0Hmc0Q3dUxPDo6Ng"
+
+// Facebook APP ID is in PSFacebookCenter.h
